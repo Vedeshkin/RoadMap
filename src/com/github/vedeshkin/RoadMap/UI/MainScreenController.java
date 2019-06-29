3bdcf170c44a7edda772051d09144725b63cfde1
vvedeshkin <evilfunt@gmail.com>
2019-06-29 09:26:07 +0300

    Updated the UI Main Idea - create separate the UI for Road and City management

--- src/com/github/vedeshkin/RoadMap/UI/MainScreenController.java
+++ src/com/github/vedeshkin/RoadMap/UI/MainScreenController.java
@@ -7,7 +7,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
-public class MainScreenController  {
+public class MainScreenController {
 
     @FXML
     private Button addCityButton;
@@ -21,17 +21,36 @@ public class MainScreenController  {
     public void handleAddCityButton(ActionEvent actionEvent) {
         String city = cityNameTextField.getText();
         int population = Integer.parseInt(cityPopulationTextField.getText());
-        if(city.isEmpty()){
+        if (city.isEmpty()) {
             System.out.println("City is empty");
             return;
         }
-        cityService.addCity(city,population);
+        cityService.addCity(city, population);
         //huh?We fucked up.
         //We don't know if we added city or not and what is cause of the issue.
+        //TODO:
+        /*Bad design of the city service
+         * We need to refactor it asap
+         * */
 
         cityNameTextField.clear();
         cityPopulationTextField.clear();
 
 
+    }
+
+    public void handleManageCities(ActionEvent actionEvent) {
+ /*     here we should show the modal windows within the list of the cities
+        however this window should block any other*/
+        System.out.println("Under development :(");
+    }
+
+    public void handleManageRoads(ActionEvent actionEvent) {
+        /*
+         * Here we should show the modal windows within the list of the roads
+         * Also please remember that a road must be connected to the cities
+
+         */
+        System.out.println("Under development :(");
     }
 }
